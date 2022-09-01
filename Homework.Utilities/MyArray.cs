cf5a07054952a95e078713b495357996d2dc5f3f
Eddanielf <vasilyevaserafima@gmail.com>
2022-09-01 13:30:20 +0300

    Homework4

---
+++ Homework.Utilities/MyArray.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Homework.Utilities
+{
+    public class MyArray
+    {
+        int[] a;
+
+        public MyArray(int[] a)
+        {
+            this.a = a;
+        }
+
+        public MyArray(int n)
+        {
+            a = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+                a[i] = rnd.Next(-5, 6);
+        }
+
+        public MyArray(int n, int e1) // заполнение массива одним значением e1
+        {
+            a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = e1;
+        }
+
+        public MyArray(int n, int from, int inter) // массив со стартовым значением и шагом
+        {
+            a = new int[n];
+            a[0] = from;
+            for (int i = 1; i < n; i++)
+                a[i] = a[i - 1] + inter;
+        }
+
+        public MyArray(string fileName)
+        {
+           string []array = LoadArrayFromFile(fileName);
+        }
+
+        public int Min
+        {
+            get
+            {
+                int min = a[0];
+                for (int i = 1; i < a.Length; i++)
+                    if (a[i] < min) min = a[i];
+                return min;
+            }
+        }
+
+        public int Max
+        {
+            get
+            {
+                int max = a[0];
+                for (int i = 1; i < a.Length; i++)
+                    if (a[i] > max) max = a[i];
+                return max;
+            }
+        }
+
+        public int CountPositive
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < a.Length; i++)
+                    if (a[i] > 0) count++;
+                return count;
+            }
+        }
+
+        public int CountNegative
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < a.Length; i++)
+                    if (a[i] < 0) count++;
+                return count;
+            }
+        }
+
+        public int Sum
+        {
+            get
+            {
+                int sum = 0;
+                for (int i = 0; i < a.Length; i++)
+                    sum = sum + a[i];
+                return sum;
+            }
+        }
+
+        public int MaxCount
+        {
+            get
+            {
+                int count = 0;
+                int max = a.Max();
+                for (int i = 0; i < a.Length; i++)
+                    if (a[i] == max) count++;
+                return count;
+            }
+        }
+
+
+        public MyArray Inverse()
+        {
+            int[] b = new int[a.Length];
+            for (int i = 0; i < a.Length; i++)
+                b[i] = -a[i];
+            return new MyArray(b);
+        }
+
+        public MyArray Multi(int k)
+        {
+            for (int i = 0; i < a.Length; i++)
+                a[i] = a[i] * k;
+            return new MyArray(a);
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (int v in a)
+                s = s + v + " ";
+            return s;
+        }
+
+        public void PrintArray()
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write($"{a[i]}\t");
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintArray(string[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write($"{a[i]}\t");
+            }
+            Console.WriteLine();
+        }
+
+        public static string [] Deplete(string x)
+        {
+            return x.Split(':');
+
+        }
+
+        public static string[] LoadArrayFromFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                //StreamWriter
+                //    WriteLine
+                StreamReader streamReader = new StreamReader(fileName);
+                string [] buf = new string [1000];
+                int count = 0;
+                //streamReader.ReadLine();
+                //streamReader.EndOfStream
+                while (!streamReader.EndOfStream)
+                {
+                    buf[count] = streamReader.ReadLine();
+                    count++;
+                }
+
+                string [] a = new string [count];
+                Array.Copy(buf, a, count);
+                streamReader.Close();
+                return a;
+
+            }
+            else
+                throw new FileNotFoundException();
+        }
+        
+    }
+}
---
+++ Homework4/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Homework.Utilities;
+
+namespace Homework4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool f = true;
+
+            while (f)
+            {
+                Tools.Header(4, "Васильева Серафима");
+                Console.WriteLine(" Задания");
+                Console.WriteLine("________________________________________________\n");
+                Console.WriteLine("1. Работа с одномерным массивов");
+                Console.WriteLine("2. Считывание логина и пароля из файла");
+                Console.WriteLine("Введите 0 для завершения работы");
+                Console.WriteLine("________________________________________________\n");
+
+                Console.Write("Введите номер задачи: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine(" ");
+
+                switch (number)
+                {
+                    case 0:
+                        f = false;
+                        break;
+
+                    case 1:
+                        Task1();
+                        break;
+
+                    case 2:
+                        Task2();
+                        break;
+
+                    default:
+                        Console.WriteLine("Некорректный номер. \n Попробуйте снова.");
+
+                        break;
+                }
+                Tools.Pause();
+                Console.Clear();
+
+            }
+
+            Console.WriteLine("Завершение программы. До свидания.");
+            Console.ReadLine();
+
+        }
+
+        static void Task1()
+        {
+            bool h = true;
+
+            while (h)
+            {
+                Console.WriteLine("Задаем массив:");
+                Console.WriteLine("1. Массив с шагом ");
+                Console.WriteLine("2. Рандомный массив ");
+                Console.WriteLine("0. Выход ");
+                Console.WriteLine("________________________________________________\n");
+
+                Console.Write("Введите номер задачи: ");
+                int number1 = int.Parse(Console.ReadLine());
+                Console.WriteLine(" ");
+
+                switch (number1)
+                {
+                    case 0:
+                        h = false;
+                        break;
+
+                    case 1:
+                        Task1_1();
+                        break;
+
+                    case 2:
+                        Task1_2();
+                        break;
+
+                    default:
+                        Console.WriteLine("Некорректный номер. \n Попробуйте снова.");
+
+                        break;
+                }
+                Tools.Pause();
+                Console.Clear();
+
+            }
+            Console.WriteLine("Нажмите любую клавишу");
+
+        }
+
+        static void Task1_1()
+        {
+            Console.Write("Введите число элементов массива: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите начальный элемент массива: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите шаг массива: ");
+            int d = int.Parse(Console.ReadLine());
+
+            MyArray a = new MyArray(n, n1, d);
+            Console.WriteLine($"Ваш массив: {a.ToString()}");
+            Console.WriteLine($"Минимальное число - {a.Min}; Максимальное - {a.Max}");
+            Console.WriteLine($"Сумма элементов массива: {a.Sum}");
+            MyArray b = a.Inverse();
+            Console.WriteLine($"Инверсированный массив: {b.ToString()}");
+            Console.Write("На какое число умножим элементы массива? ");
+            int n2 = int.Parse(Console.ReadLine());
+            a = a.Multi(n2);
+            Console.WriteLine($"Массив, умноженный на {n2}: {a.ToString()}");
+        }
+
+        static void Task1_2()
+        {
+            MyArray c = new MyArray(10);
+            Console.WriteLine($"Рандомный массив от -5 до 5: {c.ToString()}");
+            Console.WriteLine($"Минимальное число - {c.Min}; Максимальное - {c.Max}");
+            Console.WriteLine($"Сумма элементов массива: {c.Sum}");
+            MyArray b = c.Inverse();
+            Console.WriteLine($"Инверсированный массив: {b.ToString()}");
+            Console.WriteLine($"Кол-во максимальных элементов массива: {c.MaxCount}");
+        }
+
+        static void Task2()
+        {
+            string[] d = MyArray.LoadArrayFromFile(AppDomain.CurrentDomain.BaseDirectory + "Authy.txt");
+            string[] e = MyArray.Deplete(d[0]);
+            string login = e[0];
+            string password = e[1];
+            int count = 0;
+            string userLogin, userPassword;                     
+
+            do
+            {
+                Console.Write("Введите логин: ");
+                userLogin = Console.ReadLine();
+
+                Console.Write("Введите пароль: ");
+                userPassword = Console.ReadLine();
+                Console.WriteLine(" ");
+
+                if (userLogin == login && userPassword == password)
+                {
+                    Console.WriteLine("Авторизация пройдена");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный логин или пароль\n");
+                    count++;
+                }
+
+            }
+            while (count < 3);
+            if (count == 3) Console.WriteLine("Вы исчерпали все попытки.");
+
+        }
+    }
+}
