f35703e00a35848476d525553e69c42be0224475
Eddanielf <vasilyevaserafima@gmail.com>
2022-09-05 17:32:05 +0300

    Homework5

---
+++ Homework.Utilities/Message.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.Utilities
+{
+    public static class Message
+    {
+
+        private static string[] separators = { ",", ".", "!", "?", ";", ":", " " };
+
+        public static void PrintWords(string message)
+        {
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                string str = words[i];
+
+                if (words[i].Length >= 3 && words[i][0] == (words[i][words[i].Length - 1]))
+                {
+                    Console.WriteLine(words[i]);
+                }
+            }
+        }
+
+        public static void PrintWords(string message, int n)
+        {
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                string str = words[i];
+
+                if (words[i].Length <= n)
+                {
+                    Console.Write(words[i] + " ");
+                }
+            }
+        }
+
+        public static string RemoveWords(string message, string userWord)
+        {
+            if (string.IsNullOrEmpty(message))
+                return null;
+            StringBuilder stringBuilder = new StringBuilder();
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                if (word != userWord)
+                    stringBuilder.Append($"{word} ");
+            }
+
+            return stringBuilder.ToString();
+
+
+        }
+
+        public static string GetStringFromWords(string message, int minWordLenght)
+        {
+            if (string.IsNullOrEmpty(message))
+                return null;
+            StringBuilder stringBuilder = new StringBuilder();
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                if (word.Length >= minWordLenght)
+                    stringBuilder.Append($"{word} ");
+            }
+
+            return stringBuilder.ToString();
+
+        }
+
+        public static string MaxWord(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                return null;
+            
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string maxWord = words[0];
+            for (int i = 1; i < words.Length; i++)
+            {
+                if (words[i].Length > maxWord.Length)
+                {
+                    maxWord = words[i];
+                }
+            }
+
+            return maxWord;
+        }
+
+        public static string MinWord(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                return null;
+            
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string minWord = words[0];
+            for (int i = 1; i < words.Length; i++)
+            {
+                if (words[i].Length < minWord.Length)
+                {
+                    minWord = words[i];
+                }
+            }
+
+            return minWord;
+        }
+
+        public static string maxWords(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            return null;
+            StringBuilder str = new StringBuilder();
+            StringBuilder str1 = new StringBuilder();
+            string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                str.Append($"{word} ");
+            }
+
+            int n = 0;
+            do
+            {   
+                string maxWord = Message.MaxWord(str.ToString());
+                str1.Append(maxWord + " ");
+                str.Replace(maxWord, " ");
+                n++;
+            }
+            while (n < 3);
+
+            return str1.ToString();
+
+        }
+
+        public static bool CompareStrings(string str1, string str2)
+        {
+            
+            bool f = str1.Select(Char.ToLower).OrderBy(x => x).SequenceEqual(str2.Select(Char.ToLower).OrderBy(x => x));
+            return f;
+        }
+    }
+}
---
+++ Homework5/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using Homework.Utilities;
+
+namespace Homework5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool f = true;
+
+            while (f)
+            {
+                Tools.Header(5, "Васильева Серафима");
+                Console.WriteLine(" Задания");
+                Console.WriteLine("________________________________________________\n");
+                Console.WriteLine("1. Корректность ввода логина");
+                Console.WriteLine("2. Класс Message");
+                Console.WriteLine("3. Метод проверки строк");
+                Console.WriteLine("Введите 0 для завершения работы");
+                Console.WriteLine("________________________________________________\n");
+
+                Console.Write("Введите номер задачи: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine(" ");
+
+                switch (number)
+                {
+                    case 0:
+                        f = false;
+                        break;
+
+                    case 1:
+                        Task1();
+                        break;
+
+                    case 2:
+                        Task2();
+                        break;
+
+                    case 3:
+                        Task3();
+                        break;
+
+                    default:
+                        Console.WriteLine("Некорректный номер. \n Попробуйте снова.");
+
+                        break;
+                }
+                Tools.Pause();
+                Console.Clear();
+
+            }
+
+            Console.WriteLine("Завершение программы. До свидания.");
+            Console.ReadLine();
+
+        }
+
+        static void Task1()
+        {
+            Console.WriteLine(" Задание #1. Корректность ввода логина. \n");
+            Regex login = new Regex("^[a-zA-Z][a-zA-Z0-9]{1,9}$");
+
+            string userLogin;
+            
+            do
+            {
+                Console.Write("Введите логин: ");
+                userLogin = Console.ReadLine();
+                if (login.IsMatch(userLogin))
+                {
+                    Console.WriteLine("Логин корректный");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Логин некорректный.Попробуйте снова");
+                }
+            }
+            while (true);
+
+        }
+
+        static void Task2()
+        {
+            Console.WriteLine(" Задание #2. Демонстрация работы класса Message. ");
+
+            string message = "белка с утра купила булку с яблоком и сок";
+            Console.WriteLine(" ");
+            Console.WriteLine($"Сообщение: + {message}");
+
+            Console.Write("В словах должно быть x или менее букв: ");
+            int n = int.Parse(Console.ReadLine());
+            Message.PrintWords(message, n);
+            Console.WriteLine(" ");
+
+            Console.Write("Убираем слово: ");
+            string word = Console.ReadLine();
+            Console.WriteLine(Message.RemoveWords(message, word));
+            Console.WriteLine(" ");
+
+            Console.WriteLine($"Самое длинное слово: {(Message.MaxWord(message))}");
+            Console.WriteLine($"Самое короткое слово: {(Message.MinWord(message))}\t");
+
+            Console.WriteLine($"Три самых длинных слова: {Message.maxWords(message)} ");
+
+        }
+
+        static void Task3()
+        {
+            Console.WriteLine(" Задание #3. Проверка соответствия строк. "); //без учета регистра. Чисто перестановка слов
+
+            Console.Write("Введите первую строку: ");
+            string str1 = Console.ReadLine();
+            Console.Write("Введите вторую строку: ");
+            string str2 = Console.ReadLine();
+
+            bool f = Message.CompareStrings(str1, str2);
+
+            if (f == true)
+                Console.WriteLine("Строки состоят из одних и тех же символов.");
+            else
+                Console.WriteLine("Строки различны.");
+        }
+    }
+}
